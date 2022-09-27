14c15699006ad15882942e278def818af95999b5
Nikola <nikola.wesely.96@gmail.com>
2022-09-27 23:35:01 +0200

    Update tasks

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -3,6 +3,12 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from "rxjs"
 import { Task } from '../Tasks';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -19,5 +25,10 @@ export class TaskService {
   deleteTask(task:Task) : Observable<Task> {
     const url = `${this.apiUrl}/${task.id}`;
     return this.http.delete<Task>(url);
+  }
+
+  updateTaskReminder(task:Task) : Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions)
   }
 }
