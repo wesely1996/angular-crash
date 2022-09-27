b70e0700872b771785d13d302a0f83082f2b7e57
Nikola <nikola.wesely.96@gmail.com>
2022-09-28 00:17:09 +0200

    Added Routs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -10,6 +11,10 @@ import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AddTaskComponent } from './components/add-task/add-task.component';
+
+const appRouts: Routes = [
+  {path: '', component: TasksComponent}
+]
 
 @NgModule({
   declarations: [
@@ -24,7 +29,8 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
     BrowserModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRouts, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
