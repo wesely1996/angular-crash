8919aea2bacdebe568ba8f0d4d2daab17013544a
Nikola <nikola.wesely.96@gmail.com>
2022-09-27 21:18:26 +0200

    Added X at task item end

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Task } from 'src/app/Tasks';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task-item',
@@ -9,6 +10,7 @@ import { Task } from 'src/app/Tasks';
 export class TaskItemComponent implements OnInit {
 
   @Input() task: Task = {text: '', day: '', reminder: false};
+  faTimes = faTimes;
 
   constructor() { }
 
